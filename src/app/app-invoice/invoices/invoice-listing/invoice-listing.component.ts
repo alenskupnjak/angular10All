dce5a643ea103a3db76b372d9c41574c614c285a
AlenS <alenskupnjak@yahoo.com>
2020-10-21 15:31:14 +0200

    025 invoice data table

--- src/app/app-invoice/invoices/invoice-listing/invoice-listing.component.ts
+++ src/app/app-invoice/invoices/invoice-listing/invoice-listing.component.ts
@@ -1,29 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { InvoiceService } from '../../services/invoice.service';
 
+
 @Component({
   selector: 'app-invoice-listing',
   templateUrl: './invoice-listing.component.html',
   styleUrls: ['./invoice-listing.component.css'],
 })
+
+
 export class InvoiceListingComponent implements OnInit {
+  displayedColumns: string[] = ['item','qty', 'date', 'rate', 'tax'];
+  dataSource;
   constructor(public invoiceService: InvoiceService) {}
 
   ngOnInit(): void {
     this.invoiceService.getInvoices().subscribe((data) => {
-      console.log('****');
-
+      this.dataSource = data
       console.log(data);
     });
 
     this.invoiceService.fetchAllInvoices().then((data) => {
-      console.log('ajmooo');
       console.log(data);
     });
 
     this.invoiceService
       .fetchAllInvoicesAsync()
       .then((data) => {
+        console.log(data);
         return data;
       })
       .catch((err) => {
